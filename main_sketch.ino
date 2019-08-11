dcf3383877313db69fbadcd2e940809270cc5f1a
Mohit Misra <49111972+mhtmsr@users.noreply.github.com>
2019-08-12 01:52:00 +0530

    Update main_sketch.ino
    Libraries Used:
    -----------------------
    <Wire.h>
    <LiquidCrystal_I2C.h>
    RTClib.h"
    <ShiftRegister74HC595.h>

--- main_sketch.ino
+++ main_sketch.ino
@@ -2,20 +2,23 @@
 #include <LiquidCrystal_I2C.h>
 #include "RTClib.h"
 #include <ShiftRegister74HC595.h>
+///////////////////////////////////Change this Pin Number here////////////
 #define input1 49
 #define input2 45
 #define input3 53
 #define input4 47
 #define input5 51
-
+//////////////////////////////////////////////////////////////////////////////////
 DateTime now;
 char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
 RTC_DS3231 rtc;
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+////////////////////////////////////Set the pin number for Mega which ar connected to the Shift Registers//////////
 ShiftRegister74HC595 days (4,4,2,3);//Set the Pins
 ShiftRegister74HC595 dday (2,43,39,41);//Set the Pins
 ShiftRegister74HC595 months (2,16,37,14);//Set the Pins
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void showDate(void);
 void showTime(void);
 void showDay(void); 
@@ -40,7 +43,7 @@ void setup ()
     Serial.println("Couldn't find RTC Module");
     while (1);
   }
-
+/////////////////////////////////////////////////Uncomment the Lines When uploading for first time//////////////////////////
 /* if (rtc.lostPower()) 
   {
     Serial.println("RTC lost power, lets set the time!");
@@ -48,7 +51,7 @@ void setup ()
   }
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));*/
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void loop () 
 {      
@@ -60,7 +63,7 @@ void loop ()
         int i5=digitalRead(input5);
          delay(100);
         
-         if(i1==HIGH)
+         if(i1==HIGH)                         //Pressing the Set Button with a another button of + or - to change paramenters trigger this branch.
 
          {    
             
@@ -117,12 +120,12 @@ void loop ()
 }
 
 
-   void show()
+   void show()                                    //This Functions shows all the Parameters on the calendar in lcd as well as LEDs.
    {
-              showDate();
-              showDay();
-              showTime();
-              int d=now.day()-1;
+              showDate();                          //Show date in LCD
+              showDay();                           //Show day in LCD
+              showTime();                          //Show Time in LCD
+              int d=now.day()-1;                    //STORE the date as (-1)decrement is used as shift register o/p starts from number 0 not 1.
               int m=now.month()-1;
               int ddow=now.dayOfTheWeek();
               Serial.println(ddow);
@@ -130,7 +133,7 @@ void loop ()
               months.setAllLow();
               dday.setAllLow();
               if(ddow==0)
-              dday.set(7,HIGH);
+              dday.set(7,HIGH);                   
               else
               dday.set(ddow,HIGH);
               days.set(d,HIGH);
@@ -138,7 +141,7 @@ void loop ()
     
    }
 
- void showDate()
+ void showDate()                                        //This Functions print the Date on The LCD Montior.
  {
   lcd.setCursor(0,0);
   lcd.print(now.day());
@@ -151,9 +154,9 @@ void loop ()
  void showDay()
  {
   lcd.setCursor(11,0);
-  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);           //This Functions print the Day of the Week on The LCD Montior.
  }
- void showTime()
+ void showTime()                                          //This Functions print the Time on The LCD Montior.
  {
   lcd.setCursor(0,1);
   lcd.print("Time:");
